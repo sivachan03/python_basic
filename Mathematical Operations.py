66153b04fb0424daeea7c4cc768248c2a1ba8c41
Your Name <your.email@example.com>
2025-05-02 22:53:24 +0530

    first commit

---
+++ Mathematical Operations.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the first Number : "))
+num2=int(input("Enter the secound Number : "))
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print("\n Addition is :",add,"\n Subtraction is :",sub,"\n Multiplication is",mul,"\n Division" ,div)
